ca8505cf8006faba300228f496d791c6965672b3
mberzins09 <165376179+mberzins09@users.noreply.github.com>
2024-10-25 09:40:55 +0300

    Initial commit

---
+++ RankingApp/RankingApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace RankingApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
