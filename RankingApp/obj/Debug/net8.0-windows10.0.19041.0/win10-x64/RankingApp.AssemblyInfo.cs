6c455665419511d2bcf4519450c507e1455c0b88
mberzins09 <165376179+mberzins09@users.noreply.github.com>
2025-07-13 09:27:59 +0300

    com

--- RankingApp/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/RankingApp.AssemblyInfo.cs
+++ RankingApp/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/RankingApp.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RankingApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+b6accacfa4b5fbe903d3b3388279492007b4453b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+ff125207beedf9fc1fbeeceecd364230f5fe68aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("RankingApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RankingApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
