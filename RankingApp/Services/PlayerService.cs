4aa8ffde9eae8e64fdef72cdc891be519d30e164
MartinsSkyBill <martins.berzins@skybill.eu>
2025-11-04 18:14:02 +0200

    11

--- RankingApp/Services/PlayerService.cs
+++ RankingApp/Services/PlayerService.cs
@@ -283,12 +283,16 @@ namespace RankingApp.Services
             var appData = await GetAppDataAsync();
             int persistedId = appData.AppUserPlayerId;
             if (persistedId == 0)
+            {
                 return;
+            }
 
             if (appData.AppUserOldId > 0 && appData.AppUserNewId > 0)
             {
                 if (appData.AppUserOldId == persistedId || appData.AppUserNewId == persistedId)
+                {
                     return;
+                }
             }
 
             async Task<List<PlayerDB>> GetSafely(string date)
